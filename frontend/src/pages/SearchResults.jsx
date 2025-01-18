4eedd0b91ebb99f0d10a904a75c330d93fe100a7
TusharSaini999 <tusharsaini.in@gmail.com>
2025-01-18 10:17:30 +0530

    improve seach

--- frontend/src/pages/SearchResults.jsx
+++ frontend/src/pages/SearchResults.jsx
@@ -1,11 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
-import img1 from "../assets/1.jpg";
-import img2 from "../assets/2.jpg";
-import img3 from "../assets/3.jpg";
-import img4 from "../assets/4.jpg";
-import img5 from "../assets/5.jpg";
 
 const SearchResults = () => {
   const [results, setResults] = useState([]);
